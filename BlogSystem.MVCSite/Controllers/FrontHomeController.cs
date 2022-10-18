787d4c8f8539226bff632c91331b62ae09f26597
18867979070@163.com <18867979070@163.com>
2022-10-18 21:11:47 +0800

    update

---
+++ BlogSystem.MVCSite/Controllers/FrontHomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace BlogSystem.MVCSite.Controllers
+{
+    public class FrontHomeController : Controller
+    {
+
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+
+
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
