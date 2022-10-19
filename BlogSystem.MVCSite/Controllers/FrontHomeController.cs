3f472f3547caa0271237896d4a19878768ea26a4
18867979070@163.com <18867979070@163.com>
2022-10-19 21:22:48 +0800

    update

--- BlogSystem.MVCSite/Controllers/FrontHomeController.cs
+++ BlogSystem.MVCSite/Controllers/FrontHomeController.cs
@@ -14,10 +14,34 @@ namespace BlogSystem.MVCSite.Controllers
         }
 
 
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult ForgetPassword()
+        {
+            return View();
+        }
+
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
 
 
         // GET
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
         {
             return View();
         }
